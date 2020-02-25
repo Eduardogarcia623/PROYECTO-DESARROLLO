ac4aa3dcbb09a3c7054971bce122e8467269c1ee
Garvac <garvac23@gmail.com>
2020-02-24 22:54:38 -0500

    LOGIN2

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name ="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X.UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <title>
         login con sesion
     </title>
@@ -21,10 +22,10 @@
 
     <form action="logica/loguear.php" method="POST">
 
-        <div class="div1">
-
+        <div class="container col-6 " >
 
            <img src="imagenes/user1.png" width="100" height="120">
+
             <br><br>
             <label> Usuario: </label><br><br>
         <input type="text" name="usuario" placeholder="Ingrese usuario">
@@ -41,7 +42,9 @@
     </form>
 
 </center>
-
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="bootstrap/js/bootstrap.min.js" ></script>
 
 
 
